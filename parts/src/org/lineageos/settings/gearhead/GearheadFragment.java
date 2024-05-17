cdd6407b11ed319e5fffebf3346638ec362e671f
SahilSonar <sss.sonar2003@gmail.com>
2024-05-17 23:23:18 +0530

    gearhead: Wire up a service to toggle Android Auto support
    Ships as disabled on first boot
    
    Issue: calyxos#1460
    Co-authored-by: Michael Bestas <mkbestas@gmail.com>
    Change-Id: I166c671c934830ec6d1e969a61d300c2d1c7c4d9

---
+++ parts/src/org/lineageos/settings/gearhead/GearheadFragment.java
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.settings.gearhead;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
+import androidx.preference.SwitchPreference;
+
+import org.lineageos.settings.R;
+
+public class GearheadFragment extends PreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
+    private static final String PREF_GEARHEAD = "gearhead_pref";
+
+    private SwitchPreference mGearheadPref;
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.gearhead_settings, rootKey);
+
+        mGearheadPref = findPreference(PREF_GEARHEAD);
+        mGearheadPref.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGearheadPref) {
+            boolean value = (Boolean) newValue;
+            return GearheadUtils.setApplicationEnabledSetting(getActivity(), value);
+        }
+        return false;
+    }
+}
