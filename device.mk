720a4606251f5774628cd05c2ce96f95706c9c3a
Michael Bestas <mkbestas@gmail.com>
2025-02-18 01:58:00 +0200

    gearhead: Add a device makefile
    Issue: calyxos#1460
    Change-Id: Ie6dfaad425fc735ec36da34d23602a1ed3b3698f

---
+++ device.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: 2025 The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_PACKAGES += \
+    GearheadOverlay \
+    default-permissions_com.google.android.projection.gearhead \
+    privapp_permissions_com.google.android.projection.gearhead
