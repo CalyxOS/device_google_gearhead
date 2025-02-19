6ad6f7f33bab73197a01d88a9c5f8bfdf03beab9
Michael Bestas <mkbestas@gmail.com>
2025-02-19 03:23:23 +0200

    gearhead: parts: Migrate to switchbar
    This matches the MD3 Settings style
    
    Issue: calyxos#1460
    Change-Id: I184a889b520ea84bc1b252c6921a196c02a2c751

--- parts/src/org/calyxos/gearheadsupport/gearhead/GearheadFragment.java
+++ parts/src/org/calyxos/gearheadsupport/gearhead/GearheadFragment.java
@@ -7,29 +7,29 @@ package org.calyxos.gearheadsupport.gearhead;
 
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.widget.CompoundButton;
 
-import androidx.preference.Preference;
 import androidx.preference.PreferenceFragment;
-import androidx.preference.SwitchPreferenceCompat;
 
 import com.android.settingslib.HelpUtils;
+import com.android.settingslib.widget.MainSwitchPreference;
 import com.android.settingslib.widget.FooterPreference;
 
 import org.calyxos.gearheadsupport.R;
 
 public class GearheadFragment extends PreferenceFragment
-        implements Preference.OnPreferenceChangeListener {
+        implements CompoundButton.OnCheckedChangeListener {
     private static final String PREF_GEARHEAD = "gearhead_pref";
     private static final String PREF_FOOTER = "footer_preference";
 
-    private SwitchPreferenceCompat mGearheadPref;
+    private MainSwitchPreference mSwitchBar;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.gearhead_settings, rootKey);
 
-        mGearheadPref = findPreference(PREF_GEARHEAD);
-        mGearheadPref.setOnPreferenceChangeListener(this);
+        mSwitchBar = findPreference(PREF_GEARHEAD);
+        mSwitchBar.addOnSwitchChangeListener(this);
 
         FooterPreference footerPreference = findPreference(PREF_FOOTER);
         String helpUrl = getString(R.string.android_auto_help_url);
@@ -41,11 +41,7 @@ public class GearheadFragment extends PreferenceFragment
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference == mGearheadPref) {
-            boolean value = (Boolean) newValue;
-            return GearheadUtils.setApplicationHiddenSetting(getActivity(), !value);
-        }
-        return false;
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        GearheadUtils.setApplicationHiddenSetting(getActivity(), !isChecked);
     }
 }
